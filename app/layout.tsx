cd0fa59adcae64422b9410e76a2fe1caa96c84f9
ingaSiu <ingasiud@gmail.com>
2024-02-26 18:17:23 +0200

    add header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
         <StoreContent />
         <Footer />
