07afb6f96e40eba65b64e30fcdb69736d4336e37
ingaSiu <ingasiud@gmail.com>
2024-02-26 14:20:35 +0200

    add basic card for store

---
+++ components/storeContent/storeCard/StoreCard.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './StoreCard.module.scss';
+
+type CardProps = {
+  storeLink: string;
+  imageLink: string;
+  text: string;
+  storeName: string;
+};
+
+const StoreCard = ({ imageLink, text, storeName, storeLink }: CardProps) => (
+  <Link href={storeLink} target="_blank" className={styles.cardWrapper}>
+    <Image src={imageLink} alt="store" width={65} height={58} />
+    <div className={styles.wrapper}>
+      <p className={styles.text}>{text}</p>
+      <h4 className={styles.storeTitle}>{storeName}</h4>
+    </div>
+  </Link>
+);
+
+export default StoreCard;
