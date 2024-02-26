42b64b926c6e6ac8ecc62e5a549d69e99e0e8120
ingaSiu <ingasiud@gmail.com>
2024-02-26 19:09:20 +0200

    add custom fonts

---
+++ components/fonts.ts
@@ -0,0 +1,5 @@
+import { Caveat_Brush, Noto_Sans } from 'next/font/google';
+
+export const noto_sans = Noto_Sans({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const caveat_brush = Caveat_Brush({ subsets: ['latin'], weight: ['400'] });
