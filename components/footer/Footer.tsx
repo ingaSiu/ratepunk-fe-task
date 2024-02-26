1e692803d39c304ac7eb92e26438003c7fcea58e
ingaSiu <ingasiud@gmail.com>
2024-02-26 14:19:02 +0200

    add basic footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import Contact from './contact/Contact';
+import Intro from './intro/Intro';
+import Links from './links/Links';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <div className={styles.footerWrapper}>
+      <Intro />
+      <Links />
+      <Contact />
+
+      {/* <p className={styles.copyrightMobile}>© 2021 Ratepunk. All Rights Reserved.</p> */}
+    </div>
+  );
+};
+
+export default Footer;
