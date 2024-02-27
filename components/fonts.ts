1f5f39877ae0d8a68294cd58da42b84938f96a96
ingaSiu <ingasiud@gmail.com>
2024-02-27 18:37:55 +0200

    add custom font for store title

--- components/fonts.ts
+++ components/fonts.ts
@@ -1,5 +1,9 @@
 import { Caveat_Brush, Noto_Sans } from 'next/font/google';
 
+import localFont from 'next/font/local';
+
 export const noto_sans = Noto_Sans({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const caveat_brush = Caveat_Brush({ subsets: ['latin'], weight: ['400'] });
+
+export const gadugi = localFont({ src: '../public/fonts/gadugi.woff2' });
--- components/storeContent/storeCard/StoreCard.tsx
+++ components/storeContent/storeCard/StoreCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { gadugi } from '@/components/fonts';
 import styles from './StoreCard.module.scss';
 
 type CardProps = {
@@ -14,7 +15,7 @@ const StoreCard = ({ imageLink, text, storeName, storeLink }: CardProps) => (
     <Image src={imageLink} alt="store" width={68} height={60} />
     <div className={styles.wrapper}>
       <p className={styles.text}>{text}</p>
-      <h4 className={styles.storeTitle}>{storeName}</h4>
+      <h4 className={`${styles.storeTitle} ${gadugi.className}`}>{storeName}</h4>
     </div>
   </Link>
 );
