78733230256e2066589ddbf3ce80068c1d82158e
ingaSiu <ingasiud@gmail.com>
2024-02-27 10:45:24 +0200

    adjust store to responsiveness

--- components/storeContent/storeCard/StoreCard.tsx
+++ components/storeContent/storeCard/StoreCard.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 
 const StoreCard = ({ imageLink, text, storeName, storeLink }: CardProps) => (
   <Link href={storeLink} target="_blank" className={styles.cardWrapper}>
-    <Image src={imageLink} alt="store" width={65} height={58} />
+    <Image src={imageLink} alt="store" width={68} height={60} />
     <div className={styles.wrapper}>
       <p className={styles.text}>{text}</p>
       <h4 className={styles.storeTitle}>{storeName}</h4>
